29280a3f1e83c3324b9c569093e6abf0b95c9d1c
Abhimanyu Chugh <abhimanyuchugh@users.noreply.github.com>
2017-07-10 22:28:40 +0100

    Implement generic heap data structure with a custom comparer and simple unit tests for min/max heaps

---
+++ HeapDS/HeapDS.Tests/HeapTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HeapDS.Tests
+{
+    [TestClass]
+    public class HeapTest
+    {
+        [TestMethod]
+        public void TestMinHeap()
+        {
+            var comparer = System.Collections.Generic.Comparer<int>.Create((x, y) =>
+            {
+                return x == y ? 0 : x > y ? 1 : -1;
+            });
+            var nums = new List<int>() { 0, 1, 2, 4, 5, 5, 6, 7, 9, 10, 10, 15, 25, 30 };
+            IHeap<int> heap = new Heap<int>(comparer);
+            foreach (var n in nums)
+            {
+                heap.AddItem(n);
+            }
+
+            int index = 0;
+            int num;
+            while (heap.TryRemoveTopItem(out num))
+            {
+                Assert.AreEqual(nums[index], num);
+                index++;
+                int newTop;
+                var hasTop = heap.TryGetTopItem(out newTop);
+                Console.WriteLine("Item removed: {0}, New top: {1}", num, hasTop ? newTop.ToString() : "No top");
+            }
+        }
+
+        [TestMethod]
+        public void TestMaxHeap()
+        {
+            var comparer = System.Collections.Generic.Comparer<int>.Create((x, y) =>
+            {
+                return x == y ? 0 : x > y ? -1 : 1;
+            });
+            var nums = new List<int>() { 0, 1, 2, 4, 5, 5, 6, 7, 9, 10, 10, 15, 25, 30 };
+            IHeap<int> heap = new Heap<int>(comparer);
+            foreach (var n in nums)
+            {
+                heap.AddItem(n);
+            }
+
+            int index = nums.Count - 1;
+            int num;
+            while (heap.TryRemoveTopItem(out num))
+            {
+                Assert.AreEqual(nums[index], num);
+                index--;
+                int newTop;
+                var hasTop = heap.TryGetTopItem(out newTop);
+                Console.WriteLine("Item removed: {0}, New top: {1}", num, hasTop ? newTop.ToString() : "No top");
+            }
+        }
+    }
+}
---
+++ HeapDS/HeapDS/Heap.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapDS
+{
+    public interface IHeap<T>
+    {
+        bool TryGetTopItem(out T item);
+        void AddItem(T item);
+        bool TryRemoveTopItem(out T item);
+    }
+
+    public class Heap<T> : IHeap<T>
+    {
+        private readonly IComparer<T> comparer;
+        private readonly List<T> items;
+
+        public Heap(IComparer<T> comparer)
+        {
+            this.comparer = comparer;
+            this.items = new List<T>();
+        }
+
+        public bool TryGetTopItem(out T item)
+        {
+            item = default(T);
+            if (items == null || !items.Any()) return false;
+            item = items[0];
+            return true;
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+            if (items.Count <= 1) return;
+            var itemIndex = items.Count - 1;
+            // Trickle the new node up the tree until we either reach a parent which is smaller than us
+            // or we reach the root
+            while (itemIndex > 0)
+            {
+                var parentIndex = (itemIndex - 1) / 2;
+                if (comparer.Compare(items[parentIndex], items[itemIndex]) <= 0)
+                    break;
+                items[itemIndex] = items[parentIndex];
+                items[parentIndex] = item;
+                itemIndex = parentIndex;
+            }
+        }
+
+        public bool TryRemoveTopItem(out T item)
+        {
+            item = default(T);
+            if (items == null || !items.Any()) return false;
+            item = items[0];
+            var lastItem = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+            if (items.Count == 0) return true;
+            items[0] = lastItem;
+            HeapRebuild();
+            return true;
+        }
+
+        private void HeapRebuild()
+        {
+            if (items.Count <= 1) return;
+            var index = 0;
+            while (index < items.Count - 1)
+            {
+                var firstChildIndex = 2 * index + 1;
+                var secondChildIndex = 2 * index + 2;
+                // if we've reached a leaf node, no need to continue further
+                if (firstChildIndex >= items.Count && secondChildIndex >= items.Count)
+                    break;
+                int? childrenComparison = null;
+                if (firstChildIndex < items.Count && comparer.Compare(items[index], items[firstChildIndex]) > 0 &&
+                    (secondChildIndex >= items.Count || (childrenComparison = comparer.Compare(items[firstChildIndex], items[secondChildIndex])) <= 0))
+                {
+                    var item = items[index];
+                    items[index] = items[firstChildIndex];
+                    items[firstChildIndex] = item;
+                    index = firstChildIndex;
+                }
+                else if (secondChildIndex < items.Count && comparer.Compare(items[index], items[secondChildIndex]) > 0 &&
+                        (childrenComparison != null && childrenComparison > 0 ||
+                        (childrenComparison = comparer.Compare(items[firstChildIndex], items[secondChildIndex])) > 0))
+                {
+                    var item = items[index];
+                    items[index] = items[secondChildIndex];
+                    items[secondChildIndex] = item;
+                    index = secondChildIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
